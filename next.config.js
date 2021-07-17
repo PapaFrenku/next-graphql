2ec367c55d1291a874039e46e5dbe728cab420e5
Илья Овчинников <nerdik5@gmail.com>
2021-07-17 17:39:00 +0300

    added button component

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const path = require('path');
+const withSass = require('@zeit/next-sass');
+const withImages = require('next-images');
+
+module.exports = withSass({
+  cssModules: true
+});
+
+module.exports = withImages();
+
 module.exports = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')], 
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ApolloProvider } from '@apollo/client'
-import { useApollo } from '../lib/apolloClient'
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../lib/apolloClient';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps, {
     uri: process.env.HOST,
     credentials: 'same-origin'
-  })
+  });
 
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
     </ApolloProvider>
-  )
+  );
 }
-export default MyApp
+export default MyApp;
