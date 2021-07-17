cb23a944ecdf7ea5eb53d66e049706f2dd123771
Илья Овчинников <nerdik5@gmail.com>
2021-07-17 14:20:26 +0300

    added appolo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../lib/apolloClient'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const client = useApollo(pageProps, {
+    uri: process.env.HOST,
+    credentials: 'same-origin'
+  })
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 export default MyApp
